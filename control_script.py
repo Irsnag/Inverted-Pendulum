543baa8d05a3da9bc594bece0dac6f8209ece3ef
Irshath Nagouri <irsnag@gmail.com>
2025-01-26 18:11:09 +0100

    correct control

--- control_script.py
+++ control_script.py
@@ -88,3 +88,44 @@ def y_dot( t, y ):
     x_ddot = u(t, y) - m*L*y[3]*y[3] * np.cos( y[2] ) + m*g*np.cos(y[2]) *  np.sin(y[2])
     x_ddot = x_ddot / ( M+m-m* np.sin(y[2])* np.sin(y[2]) )
 
+    theta_ddot = -g/L * np.cos( y[2] ) -  np.sin( y[2] ) / L * x_ddot
+
+
+    damping_x =  - d1*y[1]
+    damping_theta =  - d2*y[3]
+
+    return [ y[1], x_ddot + damping_x, y[3], theta_ddot + damping_theta ]
+
+if __name__=="__1main__":
+
+    # Verifying the correctness of linearization by evaluating y_dot
+    # in two ways a) non-linear b) linear approximation
+
+    at_y = np.array( [0,0,np.pi/2+0.1,0.002] )
+    print 'non-linear', y_dot( 1.0, at_y )
+    print 'linearized ', np.matmul( ss.A, at_y - np.array([0,0,np.pi/2.,0]) )  + ss.B.T * 0.1
+    code.interact(local=dict(globals(), **locals()))
+
+    # See also analysis_of_linearization to know more.
+
+
+
+# Both cart and the pendulum can move.
+if __name__=="__main__":
+    # We need to write the Euler-lagrange equations for the both the
+    # systems (bob and cart). The equations are complicated expressions. Although
+    # it is possible to derive with hand. The entire notes are in media folder or the
+    # blog post for this entry. Otherwse in essense it is very similar to free_fall_pendulum.py
+    # For more comments see free_fall_pendulum.py
+    sol = solve_ivp(y_dot, [0, 20], [ 0.0, 0., np.pi/2 + 0.01, 0. ],   t_eval=np.linspace( 0, 20, 100)  )
+
+
+    syst = InvertedPendulum()
+
+    for i, t in enumerate(sol.t):
+        rendered = syst.step( [sol.y[0,i], sol.y[1,i], sol.y[2,i], sol.y[3,i] ], t )
+        cv2.imshow( 'im', rendered )
+        cv2.moveWindow( 'im', 100, 100 )
+
+        if cv2.waitKey(0) == ord('q'):
+            break
