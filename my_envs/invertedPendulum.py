b2597def75ee4ee6baace8db4ae40681d3fd206b
Irshath Nagouri <irsnag@gmail.com>
2025-02-01 18:50:10 +0100

    try different animation

--- my_envs/invertedPendulum.py
+++ my_envs/invertedPendulum.py
@@ -5,111 +5,61 @@ Created on Fri Jan 24 22:40:57 2025
 @author: 33606
 """
 
-### This file contains the drawing part for various systems. Given a state vector
-### Will return an imageself.
-
-import numpy as np
 import cv2
+import numpy as np
+import time
 
 class InvertedPendulum:
-    def __init__(self):
-        f = 0
+    def __init__(self, length=100, width=500, height=300):
+        self.length = length  # Length of the pendulum (scaled for visualization)
+        self.width = width    # Width of the window
+        self.height = height  # Height of the window
+        self.cart_width = 50
+        self.cart_height = 20
+        self.cart_x = width // 2
+        self.cart_y = height // 2 + 50
+        self.theta = np.pi / 4  # Initial angle (45 degrees)
+        self.running = True
+    
+    def update(self, theta):
+        """Update the angle of the pendulum and redraw."""
+        self.theta = theta
+    
+    def draw(self):
+        """Draw the cart and pendulum using OpenCV."""
+        frame = np.ones((self.height, self.width, 3), dtype=np.uint8) * 255
+        
+        # Cart position (centered at cart_x, cart_y)
+        cart_top_left = (self.cart_x - self.cart_width // 2, self.cart_y - self.cart_height // 2)
+        cart_bottom_right = (self.cart_x + self.cart_width // 2, self.cart_y + self.cart_height // 2)
+        cv2.rectangle(frame, cart_top_left, cart_bottom_right, (0, 0, 255), -1)
+        
+        # Pendulum end position
+        pendulum_x = int(self.cart_x + self.length * np.sin(self.theta))
+        pendulum_y = int(self.cart_y - self.length * np.cos(self.theta))
+        
+        # Draw pendulum
+        cv2.line(frame, (self.cart_x, self.cart_y), (pendulum_x, pendulum_y), (0, 255, 0), 5)
+        cv2.circle(frame, (pendulum_x, pendulum_y), 8, (255, 0, 0), -1)
+        
+        # Show frame
+        cv2.imshow("Inverted Pendulum", frame)
+        
+    def run(self):
+        """Run the animation loop."""
+        while self.running:
+            self.draw()
+            key = cv2.waitKey(50)  # 50ms delay (adjust for speed)
+            
+            # Simulate a small oscillation (just for visualization purposes)
+            self.theta += np.random.uniform(-0.05, 0.05)
+            
+            if key == 27:  # ESC key to exit
+                self.running = False
+        
+        cv2.destroyAllWindows()
 
-    def step( self, state_vec, t=None ):
-        """ state vector :
-                x0 : position of the cart
-                x1 : veclocity of the cart
-                x2 : angle of pendulum. In ref frame with x as forward of the cart and y as up. Angile with respect to ground plane
-                x3 : angular velocity of the pendulum
-        """
-        CART_POS = state_vec[0]
-        BOB_ANG  = state_vec[2]*180. / np.pi # degrees
-        LENGTH_OF_PENDULUM = 110.
-
-        IM = np.zeros( (512, 512, 3), dtype='uint8' )
-
-        # Ground line
-        cv2.line(IM, (0, 450), (IM.shape[1], 450), (19,69,139), 4 )
-
-
-        # Mark ground line
-        XSTART = -5.
-        XEND = 5.
-        for xd in np.linspace( XSTART, XEND, 11 ):
-            x = int(   (xd - XSTART) / (XEND - XSTART) * IM.shape[0]   )
-
-            cv2.circle( IM, (x, 450), 5, (0,255,0), -1 )
-
-            cv2.putText(IM, str(xd), (x-15,450+15), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (200,200,250), 1);
-
-
-        # Draw Wheels of the cart
-        wheel_1_pos = int(   (CART_POS - 1.2 - XSTART) / (XEND - XSTART) * IM.shape[0]   )
-        wheel_2_pos = int(   (CART_POS + 1.2 - XSTART) / (XEND - XSTART) * IM.shape[0]   )
-
-        cv2.circle( IM, (wheel_1_pos, 415), 25, (255,255,255), 6 )
-        cv2.circle( IM, (wheel_2_pos, 415), 25, (255,255,255), 6 )
-        cv2.circle( IM, (wheel_1_pos, 415), 2, (255,255,255), -1 )
-        cv2.circle( IM, (wheel_2_pos, 415), 2, (255,255,255), -1 )
-
-        # Cart base
-        cart_base_start = int(   (CART_POS - 2.5 - XSTART) / (XEND - XSTART) * IM.shape[0]   )
-        cart_base_end   = int(   (CART_POS + 2.5 - XSTART) / (XEND - XSTART) * IM.shape[0]   )
-
-        cv2.line( IM, (cart_base_start, 380), (cart_base_end, 380), (255,255,255), 6 )
-
-        # Pendulum hinge
-        pendulum_hinge_x = int(   (CART_POS - XSTART) / (XEND - XSTART) * IM.shape[0]   )
-        pendulum_hinge_y = 380
-        cv2.circle( IM, (pendulum_hinge_x, pendulum_hinge_y), 10, (255,255,255), -1 )
-
-
-        # Pendulum
-        pendulum_bob_x = int( LENGTH_OF_PENDULUM * np.cos( BOB_ANG / 180. * np.pi ) )
-        pendulum_bob_y = int( LENGTH_OF_PENDULUM * np.sin( BOB_ANG / 180. * np.pi ) )
-        cv2.circle( IM, (pendulum_hinge_x+pendulum_bob_x, pendulum_hinge_y-pendulum_bob_y), 10, (255,255,255), -1 )
-        cv2.line( IM, (pendulum_hinge_x, pendulum_hinge_y), (pendulum_hinge_x+pendulum_bob_x, pendulum_hinge_y-pendulum_bob_y), (255,255,255), 3 )
-
-        # Mark the current angle
-        angle_display = BOB_ANG % 360
-        if( angle_display > 180 ):
-            angle_display = -360+angle_display
-        cv2.putText(IM, "theta="+str( np.round(angle_display,4) )+" deg", (pendulum_hinge_x-15, pendulum_hinge_y-15), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (200,200,250), 1);
-
-
-        # Display on top
-        if t is not None:
-            cv2.putText(IM, "t="+str(np.round(t,4))+"sec", (15, 15), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (200,200,250), 1);
-            cv2.putText(IM, "ANG="+str(np.round(BOB_ANG,4))+" degrees", (15, 35), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (200,200,250), 1);
-            cv2.putText(IM, "POS="+str(np.round(CART_POS,4))+" m", (15, 55), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (200,200,250), 1);
-
-
-
-        return IM
-
-
-if __name__=="__main__":
-    syst = InvertedPendulum()
-
-    x = 0.
-    sx = 1.
-    theta = np.pi/3
-    stheta = np.pi/3
-    t = 0.
-    while True:
-        # x += sx*0.1
-        # theta += stheta*1.
-        # if( x > 5 ):
-        #     sx = -1.
-        # if( x < -5 ):
-        #     sx = 1.0
-
-        # theta = -9.8 / 1.5 * np.cos( t ) + 95.0 + 9.8/1.5
-
-        rendered = syst.step( [x,0,theta,0] )
-        cv2.imshow( 'im', rendered )
-
-        if cv2.waitKey(30) == ord('q'):
-            break
-
-        t += 30./1000.
+# Example usage
+if __name__ == "__main__":
+    pendulum = InvertedPendulum()
+    pendulum.run()
