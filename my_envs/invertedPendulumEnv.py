a24970c790eb2fa23aeca8f5f4ef899e0ea34249
Irshath Nagouri <irsnag@gmail.com>
2025-02-07 01:56:45 +0100

    test physics

--- my_envs/invertedPendulumEnv.py
+++ my_envs/invertedPendulumEnv.py
@@ -41,7 +41,7 @@ class InvertedPendulumEnv(gym.Env):
 
         # Rendering
         self.render_mode = render_mode
-        self.state = [0, 0., 0.1, 0]
+        self.state = [0, 0., -np.pi+0.01, 0]
 
         # For rendering
         self.screen = None
@@ -50,7 +50,7 @@ class InvertedPendulumEnv(gym.Env):
     def reset(self, seed=None, options=None):
         # Reset the environment
         super().reset(seed=seed)
-        self.state = [0, 0., 0, 0]
+        self.state = [0, 0., -np.pi/2, 0]
         return np.array(self.state, dtype=np.float32), {}
 
     def step(self, action):
