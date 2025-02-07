d699f1ee8c67309063a61858b6c360f5c21dfbea
Irshath Nagouri <irsnag@gmail.com>
2025-02-07 03:37:28 +0100

    dqn and replay buffer

--- dqn.py
+++ dqn.py
@@ -5,3 +5,35 @@ Created on Fri Feb  7 03:32:57 2025
 @author: 33606
 """
 
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import random
+from collections import deque
+
+class DQN(nn.Module):
+    def __init__(self, state_size, action_size):
+        super(DQN, self).__init__()
+        self.fc1 = nn.Linear(state_size, 64)
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, action_size)  # Output Q-values for each action
+
+    def forward(self, state):
+        x = torch.relu(self.fc1(state))
+        x = torch.relu(self.fc2(x))
+        return self.fc3(x)
+
+class ReplayBuffer:
+    def __init__(self, capacity, batch_size):
+        self.memory = deque(maxlen=capacity)
+        self.batch_size = batch_size
+
+    def add(self, experience):
+        self.memory.append(experience)
+
+    def sample(self):
+        batch = random.sample(self.memory, self.batch_size)
+        return zip(*batch)
+
+    def __len__(self):
+        return len(self.memory)
--- my_envs/__init__.py
+++ my_envs/__init__.py
@@ -3,5 +3,6 @@ from gymnasium.envs.registration import register
 register(
     id="InvertedPendulumEnv-v1",  # Unique identifier for the environment
     entry_point="my_envs.invertedPendulumEnv:InvertedPendulumEnv",  # Path to your environment
+    max_episode_steps=500,
 )
 
